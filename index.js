110243954e06722124b02b12320bf56ef2eb69a2
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2024-11-04 20:31:26 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, names) {
+    const lowerCaseName = names.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowerCaseName);
+}
+function fuzzyMatch(drivers, query) {    
+    const lowerCaseQuery = query.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowerCaseQuery));
+}
+function matchName(drivers, name) {
+    const lowerCaseName = name.toLowerCase();
+    return drivers.filter(driver => driver.name.toLowerCase() === lowerCaseName);
+}
